396dfe98a7643bee7fbf9ca0a05b58bbf971e48d
EgzonKrasniqi9 <egzon2008krasniqi1@gmail.com>
2025-11-25 17:34:21 +0100

    comit

--- Ds-themes/footer.php
+++ Ds-themes/footer.php
@@ -1,7 +1,11 @@
 
 
     <footer>
+          <?php wp_nav_menu(array('theme_location' => 'wp_devs_footer', 'depth' => 1)); ?>
         <p>Digital school</p>
+        <nav class="footer-menu">
+       
+        </nav>
     </footer>
 </body>
 </html>
