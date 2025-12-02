bddca779a5cccab1c3b9d176a23f57a95392452f
EgzonKrasniqi9 <egzon2008krasniqi1@gmail.com>
2025-12-02 17:22:23 +0100

    comit

--- Ds-themes/parts/content-single.php
+++ Ds-themes/parts/content-single.php
@@ -1,20 +1,21 @@
-<article id="post-<?php the_ID();?>" <?php post_class();?>>
+<article id="post-<?php the_ID();?>" <?php post_class();?>> 
+    
     <header>
-        <h2><a href="<?php the_permalink?>"> <?php the_title();?></a></h2>
-        <?php if('post' == get_post_type()):?>
-            <div class="meta-info">
+        <h1><?php the_title();?></h1>
+          <div class="meta-info">
                 <p> Posted in <?php echo get_the_date()?> by <?php the_author_posts_link();?></p>
-                <?php if( has_category()): ?>
+                  <?php if( has_category()): ?>
                     <p>Categories: <?php the_category('');?></p>
                     <?php endif; ?>
                     <?php if( has_tags()):?>
                         <p> Tags: <?php the_tage('',',');?></p>
                         <?php endif;?>
-            </div>
-            <?php endif;?>
+                </div>
     </header>
-    <div class="content">
-        <?php the_excerpt();?>
-    </div>
+        <div class="content">
+            <?php the_content(); ?>
+            <?php wp_link_pages(); ?>
+        </div>
+
 
 </article>
